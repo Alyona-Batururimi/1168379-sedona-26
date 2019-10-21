e7980db6fdfe423007aa440a08ca1973023afd9f
Alyona Batururimi <abatururimi@gmail.com>
2019-10-21 12:36:34 +0300

    Добавляет сетку и декоративные элементы на главной

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var button=document.querySelector(".search-open-button");
+var modal=document.querySelector(".search-form-show");
+var empty=document.querySelector(".search-form-container");
+
+button.addEventListener("click", function(){
+  modal.classList.add("search-form-container")
+});
+
+form.classList.toggle("search-form-show")
+m
