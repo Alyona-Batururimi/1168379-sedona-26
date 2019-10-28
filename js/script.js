c29bd18a882d8e88c2f02c53a42fa6d898b9c6fb
Alyona Batururimi <abatururimi@gmail.com>
2019-10-28 11:55:22 +0300

    Добавляет js и анимацию для формы на главной

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 var button=document.querySelector(".search-open-button");
-var modal=document.querySelector(".search-form-show");
-var empty=document.querySelector(".search-form-container");
+var search_container = document.querySelector(".search-container");
+var is_open = true;
 
-button.addEventListener("click", function(){
-  modal.classList.add("search-form-container")
-});
+button.addEventListener("click", function(evt){
+  evt.preventDefault();
+  if (is_open){
+    search_container.classList.remove("search-form-show");
+    search_container.classList.add("search-form-hide");
+  }
+  else{
+    search_container.classList.remove("search-form-hide");
+    search_container.classList.add("search-form-show");
+  }
+  is_open = !is_open;
 
-form.classList.toggle("search-form-show")
-m
+})
